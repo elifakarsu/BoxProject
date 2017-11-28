5afd9adf769940cc8a659e61de9690faee668049
Elif Akarsu <elif0441@edu.easj.dk>
2017-11-28 14:25:23 +0100

    Elif made Customer,Box,Payment classes

---
+++ BoxProject/Model/Box.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxProject.Model
+{
+    class Box
+    {
+        private string _name;
+        private List<string> _category;
+                                                                                                                                                                                                                                                                                                  
+     }
+}
---
+++ BoxProject/Model/PaymentDetails.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxProject.Model
+{
+    class PaymentDetails
+    {
+        private string _cardNumber;
+        private string _cardName;
+        private string _expiryDate;
+        private string _securityNumber;
+
+        public string CardNumber
+        {
+            get => _cardNumber;
+            set => _cardNumber = value;
+        }
+
+        public string CardName
+        {
+            get => _cardName;
+            set => _cardName = value;
+        }
+
+        public string ExpiryDate
+        {
+            get => _expiryDate;
+            set => _expiryDate = value;
+        }
+
+        public string SecurityNumber
+        {
+            get => _securityNumber;
+            set => _securityNumber = value;
+        }
+
+        public PaymentDetails(string cardNumber, string cardName,string expiryDate, string securityNumber)
+        {
+            _cardNumber = cardNumber;
+            _cardName = cardName;
+            _expiryDate = expiryDate;
+            _securityNumber = securityNumber;
+            
+        }
+
+        public PaymentDetails()
+        {
+            
+        }
+
+    }
+}
