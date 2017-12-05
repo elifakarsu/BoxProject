37ef638d8658911cf43f910a475fac7567b11b93
Seuma Nergis <seum0001@edu.easj.dk>
2017-12-05 14:15:45 +0100

    CustomerCollection

---
+++ BoxProject/ViewModel/CustomerCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoxProject.Model;
+
+namespace BoxProject.ViewModel
+{
+    class CustomerCollection
+    {
+        private readonly FrameNavigate _frameNavigate;
+        // source property : for selected item
+        private Customer _selectedItem;
+        private readonly Singleton _userSingleton;
+
+        public ObservableCollection<Customer> Student { get; set; }
+
+        public Customer AddNewCustomer { get; set; }
+
+        public CustomerCollection()
+        {
+            AddNewCustomer = new Customer();
+
+            // Frame Navigate Object instance
+            _frameNavigate = new FrameNavigate();
+
+            // User singelton 
+            _userSingleton = Singleton.GetInstance();
+        }
+    }
+}
