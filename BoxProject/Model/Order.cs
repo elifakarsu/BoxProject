03a6cb6b7de5fa835ee2a66bad75706ed970a3a5
Seuma Nergis <seum0001@edu.easj.dk>
2017-12-05 21:56:44 +0100

    Order class added.

---
+++ BoxProject/Model/Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxProject.Model
+{
+    class Order
+    {
+        private ObservableCollection<Box> _boxes;
+        private Customer _customer;
+        private PaymentDetails _paymentDetails;
+
+        public PaymentDetails PaymentDetails { get => _paymentDetails; set => _paymentDetails = value; }
+        public ObservableCollection<Box> Boxes { get => _boxes; set => _boxes = value; }
+        public  Customer Customer { get => _customer; set => _customer = value; }
+
+        public Order()
+        {
+            
+        }
+
+        public Order(ObservableCollection<Box> boxes, Customer customer, PaymentDetails paymentdetails)
+        {
+            _boxes = boxes;
+            _customer = customer;
+            _paymentDetails = paymentdetails;
+        }
+
+        }
+    }
+}
